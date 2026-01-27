1cadee863aa898256e828e54ea11e4e3646334d2
karbasappa018 <karbasappatadakale@gmail.com>
2026-01-27 18:23:25 +0530

    Display string using array parameter in function

---
+++ Day_12/program216.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void Display(char str[])
+{
+    while(*str != '\0')
+    {
+        printf("%c",*str);
+        str++;
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);
+    
+    Display(Arr);
+
+    return 0;
+}
